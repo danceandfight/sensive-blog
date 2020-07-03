2ebbe3385521847384d06e9aab1da17dad50d4f1
aeon <danceandfight@gmail.com>
2020-07-03 15:27:48 +0300

    Reduce queries for comments and posts in post_detail

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 from django.db.models import Count
 
-
+"""
 def get_related_posts_count(tag):
     return tag.posts.count()
 
@@ -19,7 +19,7 @@ def serialize_post(post):
         "tags": [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
-
+"""
 def serialize_post_optimised(post):
     return {
         "title": post.title,
@@ -32,13 +32,13 @@ def serialize_post_optimised(post):
         "tags": [serialize_tag_optimised(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
-
+"""
 def serialize_tag(tag):
     return {
         'title': tag.title,
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
-
+"""
 def serialize_tag_optimised(tag):
     return {
         'title': tag.title,
@@ -58,7 +58,6 @@ def index(request):
                               .fetch_with_comments_count()
     most_fresh_posts = list(fresh_posts)[-5:]
 
-
     most_popular_tags = Tag.objects.popular()[:5].annotate(posts_count=Count('posts'))
 
     context = {
@@ -71,7 +70,7 @@ def index(request):
 
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
-    comments = Comment.objects.filter(post=post)
+    comments = Comment.objects.filter(post=post).prefetch_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
@@ -82,7 +81,7 @@ def post_detail(request, slug):
 
     likes = post.likes.all()
 
-    related_tags = post.tags.all()
+    related_tags = post.tags.annotate(posts_count=Count('posts'))
 
     serialized_post = {
         "title": post.title,
@@ -93,7 +92,7 @@ def post_detail(request, slug):
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
-        "tags": [serialize_tag(tag) for tag in related_tags],
+        "tags": [serialize_tag_optimised(tag) for tag in related_tags],
     }
     
     most_popular_tags = Tag.objects.popular()[0:5]
@@ -106,7 +105,7 @@ def post_detail(request, slug):
     
     context = {
         'post': serialized_post,
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'popular_tags': [serialize_tag_optimised(tag) for tag in most_popular_tags],
         'most_popular_posts': [serialize_post_optimised(post) for post in most_popular_posts],
     }
     return render(request, 'post-details.html', context)
