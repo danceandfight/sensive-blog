84eafc6025a7136448fcca7a4eb74b0204ad4dee
danceandfight <danceandfight@gmail.com>
2020-07-20 11:24:00 +0300

    Catch DoesNotExist exception

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 
 def serialize_post(post):
     return {
@@ -45,7 +46,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)
     comments = Comment.objects.filter(post=post).prefetch_related('author')
     serialized_comments = []
     for comment in comments:
@@ -88,7 +89,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
     
     most_popular_tags = Tag.objects.popular()[:5]
     most_popular_posts = Post.objects.popular() \
